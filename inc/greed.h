578406e33314c5baa3abf165e63b9ec08511bdf6
Joshua Tang <joshua.y.tang@gmail.com>
2016-11-29 00:03:24 -0800

    major refactoring: main moved to greed and vr code to greed_vr

---
+++ inc/greed.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <stdlib.h>
+#include <iostream>
+
+#include "shader_manager.h"
+#include "scene.h"
+
+#include "greed_vr.h"
+
+class Greed
+{
+private:
+	GLFWwindow *window;
+	ShaderManager *shader_manager;
+	bool vr_on = false;
+
+	static Scene *scene;
+	static bool lmb_down;
+	static bool rmb_down;
+	static glm::vec3 last_cursor_pos;
+
+	void vr_render();
+	void setup_scene();
+	void setup_callbacks();
+	void setup_opengl();
+	void setup_shaders();
+	void destroy();
+	/* static callbacks */
+	static void error_callback(int error, const char* description);
+	static void resize_callback(GLFWwindow* window, int width, int height);
+	static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+	static void cursor_position_callback(GLFWwindow* window, double x_pos, double y_pos);
+	static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+	static void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+public:
+	Greed();
+	~Greed();
+	void go();
+};
