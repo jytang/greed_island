eb56c47f893448dac4047f577acc5390d28c9993
Varanon A Pukasamsombut <vpukasam@UCSD.EDU>
2016-11-30 23:18:21 -0800

    Diamond square algorithm fixed

--- inc/terrain.h
+++ inc/terrain.h
@@ -12,11 +12,11 @@ private:
 	static std::vector< std::vector<GLfloat> > height_map;
 
 	static void diamond_square(unsigned int step, unsigned int size, float scale);
-	static void diamond_step(unsigned int x, unsigned int y, unsigned int size, float scale);
-	static void square_step(unsigned int x, unsigned int y, unsigned int size, float scale);
+	static void diamond_step(unsigned int x, unsigned int y, unsigned int step, unsigned int size, float scale);
+	static void square_step(unsigned int x, unsigned int y, unsigned int step, unsigned int size, float scale);
 
 public:
-	static void generate_height_map(GLuint size, GLfloat max_height, GLint village_diameter, GLuint seed);
+	static void generate_height_map(GLuint size, GLfloat max_height, GLint village_diameter, GLfloat scale, GLuint seed);
 	static float height_lookup(int x, int z);	
 };
 
