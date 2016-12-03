7209a87c13d12dacbe737786e5cf97aac2f98007
Varanon A Pukasamsombut <vpukasam@UCSD.EDU>
2016-12-02 20:05:07 -0800

    idk tres

--- src/tree.cpp
+++ src/tree.cpp
@@ -1,6 +1,7 @@
 #include "tree.h"
 
-SceneModel *Tree::tree_model;
+SceneModel *Tree::branch_model;
+SceneModel *Tree::leaf_model;
 Mesh Tree::branch_mesh;
 Mesh Tree::leaf_mesh;
 unsigned int Tree::leaf_threshold = 10;
@@ -9,9 +10,9 @@ SceneTransform *Tree::translate;
 SceneTransform *Tree::rotate;
 SceneTransform *Tree::scale;
 
-SceneModel *Tree::generate_tree(Scene *scene, Geometry *base, unsigned int num_iterations, int seed = 0)
+SceneGroup *Tree::generate_tree(Scene *scene, Geometry *base, unsigned int num_iterations, int seed = 0)
 {	
-	//SceneGroup *tree_group = new SceneGroup(scene);
+	SceneGroup *tree_group = new SceneGroup(scene);
 
 	//Set Random Seed
 	if (seed == 0)
@@ -25,13 +26,18 @@ SceneModel *Tree::generate_tree(Scene *scene, Geometry *base, unsigned int num_i
 	leaf_material.diffuse = leaf_material.ambient = color::windwaker_green;	
 	branch_mesh = { base, branch_material, ShaderManager::get_default(), glm::mat4(1.0f)};
 	leaf_mesh = { base, leaf_material, ShaderManager::get_default() , glm::mat4(1.0f) };
-	tree_model = new SceneModel(scene);
+	branch_model = new SceneModel(scene);
+	leaf_model = new SceneModel(scene);
 
-	//tree_group->add_child(branch_model); //Base of tree BUT IT'S NOT SCALED RIGHT
+	tree_group->add_child(branch_model); //Base of tree BUT IT'S NOT SCALED RIGHT
+	tree_group->add_child(leaf_model);
 	
 	tree_system(glm::mat4(1.0f), 0.f, 10.f, 1, num_iterations);
 
-	return tree_model;
+	branch_model->combine_meshes();
+	leaf_model->combine_meshes();
+
+	return tree_group;
 }
 
 //Uses an iterated function system (IFS)
@@ -49,7 +55,7 @@ void Tree::tree_system(glm::mat4 last_trans, float last_angle, float last_scale,
 	r *= (float)(rand() % 101 / 100.f);
 	//fprintf(stderr, "Random: %f\n", r);
 	float angle = (r * 20.0f);
-	float scale_value = last_scale * 0.6f;	
+	float scale_value = last_scale;	
 
 	glm::mat4 rot_mat = glm::rotate(glm::mat4(1.f), angle, glm::vec3(0.0f, 0.0f, 1.0f));
 	glm::vec3 dir = glm::vec3(rot_mat * glm::vec4(0.0f, scale_value * 2, 0.0f, 1.0f)); //Maybe should divide by fourth element instead
@@ -64,13 +70,18 @@ void Tree::tree_system(glm::mat4 last_trans, float last_angle, float last_scale,
 	
 	Mesh mesh;
 	if (curr_iter < leaf_threshold)
+	{
 		mesh = branch_mesh;
+		mesh.to_world = combined_mat;
+		branch_model->add_mesh(mesh);
+	}
 	else
-		mesh = leaf_mesh;	
-	mesh.to_world = combined_mat;	
-	tree_model->add_mesh(mesh);
+	{
+		mesh = leaf_mesh;
+		mesh.to_world = combined_mat;
+		leaf_model->add_mesh(mesh);
+	}		
 	
-
 	/*
 	//Attach Transformations
 	if (curr_iter < leaf_threshold)
